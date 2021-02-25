49d11373e2423f3bb18ba95055685a2e43ad3cff
neil-cartwright <neil@sitepages.co.uk>
2021-02-25 08:36:56 +0000

    changed config

--- nuxt.config.js
+++ nuxt.config.js
@@ -4,7 +4,7 @@ export default {
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: "nuxt-bulma",
+    title: "DVD Collection",
     htmlAttrs: {
       lang: "en"
     },
